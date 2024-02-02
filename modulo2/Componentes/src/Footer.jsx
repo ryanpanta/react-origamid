a8b45bf9f9da03ff982b5d1e3fc5185d80a82b71
Ryan Rodrigues <ryan.rodrigues62@hotmail.com>
2024-02-02 13:24:28 -0300

    removido a pasta modulo2 da branch modulo1

--- modulo2/Componentes/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Footer() {
-  return (
-    <footer><p>Todos os direitos reservados &copy;</p></footer>
-  )
-}    
-
-export default Footer
--- modulo2/Propriedades/src/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = ({logado, nome}) => { 
-     if(logado)
-          return <header>Bem vindo, {nome}</header>
-     return <header>Tyrolip Basics</header>
-}
-
-export default Header;
