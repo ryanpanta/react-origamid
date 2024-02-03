49830d71326e4f009f4701132e61c8b00a5f54dd
Ryan Rodrigues <ryan.rodrigues62@hotmail.com>
2024-02-02 23:14:45 -0300

    remove other folders from the modulo3 branch

--- modulo2/Ferramentas de Automação/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import React from 'react'
-
-function App(){
-    return <h1>Teste</h1>
-}
-
-ReactDom.createRoot(document.getElementById('root')).render(<App />)
