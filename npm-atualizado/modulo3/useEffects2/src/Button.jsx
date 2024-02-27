f68da8fa94d008e81dcc27b22612c062b1365611
Ryan Rodrigues <ryan.rodrigues62@hotmail.com>
2024-02-27 00:19:50 -0300

    npm-atualizado folder deleted

--- npm-atualizado/modulo3/useEffects2/src/Button.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-
-function Button({callback, titulo}) {
-  return (
-    <button style={{marginRight: "10px"}} onClick={callback}>{titulo}</button>
-  )
-}
-
-export default Button
